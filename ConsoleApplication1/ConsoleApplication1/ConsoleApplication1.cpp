4c814accb7f26afc13f7d32692b708641bf68a2f
CHJ\CHJ <kjwsx23@gmail.com>
2017-07-18 22:24:18 +0900

    3.4.6 멤버 함수 다중 정의 - 멤버 함수 다중 정의 1

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,14 +2,35 @@
 //
 
 #include "stdafx.h"
-#include "MyString.h"
 
+class C {
+public:
+	C() : N(0) {};
+	~C() {};
+
+	int Get() const {
+		return N;
+	}
+	void Set(int P) {
+		N = P;
+	}
+	void Set(double P) {
+		N = 0;
+	}
+private:
+	int N = 0;
+};
 
 int main()
 {
-	CMyString strData;
-	strData.SetString("³ª¶ù¸»½Î¹Ì µä±Î¿¡ ´Þ¾Æ");
-	cout << strData.GetString() << endl;
+	C a;
+	a.Set(10);
+	cout << a.Get() << endl;
+
+	C b;
+	b.Set(5.5);
+	cout << b.Get() << endl;
+	
 
     return 0;
 }
