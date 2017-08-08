5c1164ce327342bc89a11286c5e60fc621c97e4c
CHJ\CHJ <kjwsx23@gmail.com>
2017-08-08 16:39:14 +0900

    6.1 - 상속 클래스 기본

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,36 +2,52 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
 class C {
 public:
-	C() { cout << "C()" << endl; }
-	~C() {}
+	C() {
+		cout << "C()" << endl; 
+	}
+	~C() {
+		cout << "~C()" << endl;
+	}
 
-	C(const C &rhs) {
-		this->N = rhs.N;
+	C(const C &rhs) : N(rhs.N) {
 		cout << "C(const C &rhs)" << endl;
 	}
-	int Get() {
+	int Get() const {
 		return N;
 	}
 	void Set(int P) {
 		N = P;
 	}
-	void Set(double P) = delete;
+protected:
+	void Print() {
+		cout << "C::Print()" << endl;
+	}
 private:
-	int N = 0;
+	int N=0;
 };
 
-int main()
-{
-	C a;
-	a.Set(10);
-	cout << a.Get() << endl;
+class C2 :public C {
+public:
+	C2() {
+		cout << "C2()" << endl;
+	}
+	void T() {
+		Print();
+		Set(5);
+		cout << Get() << endl;
+	}
+};
 
-	C b(a);
-	cout << b.Get() << endl;
-	
+int main(){
+	C2 data;
+	data.Set(10);
+	cout << data.Get() << endl;
+
+	data.T();
 
     return 0;
 }
